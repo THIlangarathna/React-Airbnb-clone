5187ab27e74c52ba6f646aec609656df34c8e5bb
Thilina Ilangarathna <thilinailangarathnadev@gmail.com>
2024-02-15 09:40:39 +0530

    Create AirBnb clone

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Card from './components/Card';
+import data from './data'
 
 function App() {
+  const cardElements = data.map(card => {
+    return <Card
+      key={card.id}
+      item={card}
+    />
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="cards-list">
+        {cardElements}
+      </section>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Card(props) {
+    let badgeText;
+    if (props.item.openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (props.item.location == 'Online') {
+        badgeText = 'Online'
+    }
+    return (
+        <div className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={props.item.coverImg} className="card--image" />
+            <div className="card-stats">
+                <img className="card--star" src='../images/star.png' />
+                <span>{props.item.stats.rating}</span>
+                <span className="gray">({props.item.stats.reviewCount}) . </span>
+                <span className="gray">{props.item.location}</span>
+            </div>
+            <p>{props.item.title}</p>
+            <p><span className="bold">From ${props.item.price}</span> / person</p>
+        </div>
+    )
+}
